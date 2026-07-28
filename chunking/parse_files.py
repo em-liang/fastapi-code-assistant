1269fd4355a7fbcea155f8eb2752aaea6d709993
Emily <emilyl2017.528@gmail.com>
2026-07-27 23:14:37 -0400

    Started adding functionality to extract useful chunk information from AST

--- chunking/parse_files.py
+++ chunking/parse_files.py
@@ -14,4 +14,21 @@ with open(os.path.join(file_location, '..', 'ingest', 'files-used.txt'), "r") as
         py = file.read()
         code = py.encode('utf-8')
         tree = parser.parse(code)
-        print(tree.root_node)
+        #print(tree.root_node)
+
+def return_type(tree):
+  t = tree
+  if hasattr(t, 'root_node'): t = tree.root_node
+  if len(t.children) == 0: return
+  else:
+    for node in t.children:
+        if node.type == 'class_definition' or node.type == 'function_definition' or node.type == 'decorated_definition':
+          print(node.type)
+        return_type(node)
+  
+with open(os.path.join(file_location, '..', 'ingest', './fastapi-repo/fastapi/applications.py'), 'r') as file:
+  py = file.read()
+  code = py.encode('utf-8')
+  tree = parser.parse(code)
+  print(tree.root_node.children[0].parent.type)
+  return_type(tree)
